ff44d3e8bd175982d230e91baa8655df5a4d41e6
Samba Seye Diagne <102988205+ssdiagne1@users.noreply.github.com>
2024-02-06 01:05:13 -0500

    navBar_creation

---
+++ src/playerStats/PlayerStats.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/playerStats.css";
+
+const playerStats = () => {
+    return (
+        <div id="playerStats">
+            <h1>Samba Seye Diagne</h1>
+            <div id="playerStats__lines">
+                <span id = "player__lines__thick"></span>
+                <span id = "player__lines__thin"></span>
+            </div>
+            <h2>Junior Software Engineer</h2>
+        </div>
+    )
+
+};
+
+export default playerStats;
