877a8577f9a10ffcbbbc942555e955fee1a53e7a
Samba Seye Diagne <102988205+ssdiagne1@users.noreply.github.com>
2024-02-17 01:06:02 -0500

    v0217

--- src/avatar/Avatar.js
+++ src/avatar/Avatar.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import PropTypes from "prop-types";
-import avatarImage from "../assets/avatar-focus.png";
+import avatarImage from "../assets/playerA.png";
 import "../styles/avatar.css";
 
 const Avatar = ({ page }) => {
